291e5fbdb4b86eb485f3bcdfd808898eb4058b28
liyu10000 <yuli00986081@gmail.com>
2020-04-24 14:37:17 -0400

    add control over testing first batch; ignore sh files

--- solver.py
+++ solver.py
@@ -42,6 +42,7 @@ class Solver(object):
         self.resume_iters = config.resume_iters
 
         # Test configurations.
+        self.test_1st_batch = config.test_1st_batch
         self.test_iters = config.test_iters
 
         # Miscellaneous.
@@ -368,3 +369,6 @@ class Solver(object):
                 result_path = os.path.join(self.result_dir, '{}-images.jpg'.format(i+1))
                 save_image(self.denorm(x_concat.data.cpu()), result_path, nrow=1, padding=0)
                 print('Saved real and fake images into {}...'.format(result_path))
+
+                if self.test_1st_batch:
+                    break
